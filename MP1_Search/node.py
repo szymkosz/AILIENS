f24c64fa10579a99b4ff3126c9f021b7fec63ef0
szymkosz <szymkosz@icloud.com>
2018-02-12 20:55:05 -0600

    Removed unnecessary comments

--- MP1_Search/node.py
+++ MP1_Search/node.py
@@ -14,18 +14,6 @@ class Node:
         # This variable is only for the A* algorithms in parts 1.1 and 1.2.
         self.pathCost = float("inf")
 
-        #self.mazeIndex = mazeIndex
-
-        # Stores the nodes of the pellets to allow nodes to be uniquely distinguished
-        # by both remaining pellet configurations and Pacman's location.
-        #
-        # This variable is only for the A* algorithm in part 1.2.
-        #self.food = []
-
-        # This instance variable stores the cost of the Minimum Spanning Tree (MST)
-        # formed by the remaining pellets represented by this Node.
-        #self.MSTCost = float("inf")
-
     # Specific-use node representation (Characters only)
     def __repr__(self):
         return "{0}".format(self.char)
@@ -33,27 +21,3 @@ class Node:
     # String representation of node -> (x, y) char
     def __str__(self):
         return "({0}, {1}) {2}".format(self.x, self.y, self.char)
-
-"""
-nodeA = Node(1, 2, ' ')
-nodeB = Node(2, 3, ' ')
-
-pelletA = Node(5, 4, '.')
-pelletB = Node(2, 6, '.')
-pelletC = Node(5, 1, '.')
-
-nodeA.food.append(pelletA)
-#nodeA.food.append(pelletB)
-nodeA.food.append(pelletC)
-
-nodeB.food.append(pelletB)
-nodeB.food.append(pelletA)
-
-print(nodeA.food)
-print(nodeB.food)
-
-if set(nodeA.food) == set(nodeB.food):
-    print("The food arrays are equal.")
-else:
-    print("The food arrays are NOT equal.")
-"""
