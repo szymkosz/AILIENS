5075cd58ebfd181a7a15c3a985c6b145f9db2512
szymkosz <szymkosz@icloud.com>
2018-03-01 18:54:57 -0600

    Merge changes

--- MP2_Planning_Games/main_part2.py
+++ MP2_Planning_Games/main_part2.py
@@ -1,6 +1,6 @@
 """
 ------------------------------------------------------------------------------
-This file is for running the different agent match-ups. 
+This file is for running the different agent match-ups.
 
 To run a particular match-up for part 2.2,
 run the following command:
@@ -21,8 +21,6 @@ where:
 """
 
 from gomoku import Gomoku
-<<<<<<< HEAD
-=======
 import time
 import agents
 
@@ -39,26 +37,25 @@ if __name__ == "__main__":
 
     # Agent 1
 	if sys.argv[1] == "alpha-beta" or sys.argv[1] == "ab":
-		red = 
+		red =
 	elif sys.argv[1] == "minimax" or sys.argv[1] == "mm":
-		red = 
-	elif sys.argv[1] == "reflex" or sys.argv[1] == "r":  
-		red = 
+		red =
+	elif sys.argv[1] == "reflex" or sys.argv[1] == "r":
+		red =
 	# elif sys.argv[1] == "human" or sys.argv[1] == "h":
-	# 	red = 
+	# 	red =
 	else:
 		sys.exit("AgentNotFoundError: Is the agent name spelled correctly?")
->>>>>>> cf2500490fc7a4434e3e13ab97cf38271f223610
 
 	# Agent 2
 	if sys.argv[2] == "alpha-beta" or sys.argv[2] == "ab":
-		blue = 
+		blue =
 	elif sys.argv[2] == "minimax" or sys.argv[2] == "mm":
-		blue = 
-	elif sys.argv[2] == "reflex" or sys.argv[2] == "r": 	  
-		blue = 
+		blue =
+	elif sys.argv[2] == "reflex" or sys.argv[2] == "r":
+		blue =
 	# elif sys.argv[2] == "human" or sys.argv[2] == "h":
-	# 	blue = 
+	# 	blue =
 	else:
 		sys.exit("AgentNotFoundError: Is the agent name spelled correctly?")
 
@@ -68,19 +65,11 @@ if __name__ == "__main__":
 moves = [(3, 4), (3, 3), (4, 5), (4, 3), (5, 3)]
 
 # Random Moves
-<<<<<<< HEAD
 for m in moves:
     game.setPiece(m[0], m[1], game.reds_turn)
 
 game.setPiece(5,5, game.reds_turn)
 
 # game.setPiece(4,4,not game.reds_turn)
-=======
-# game.setPiece(3,4,1)
-# game.setPiece(3,3,0)
-# game.setPiece(4,5,1)
-# game.setPiece(4,3,0)
-# game.setPiece(5,3,1)
->>>>>>> cf2500490fc7a4434e3e13ab97cf38271f223610
 
 # print(game)
