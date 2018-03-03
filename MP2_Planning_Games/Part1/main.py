cedfa96929fa1f15eea193919dce3a6e76d820ed
tdog191 <twarther17@gmail.com>
2018-03-02 23:56:56 -0600

    Completed almost all code for Part 1

--- MP2_Planning_Games/Part1/main.py
+++ MP2_Planning_Games/Part1/main.py
@@ -1,23 +1,24 @@
-#from widget import Widget
+"""
+-------------------------------------------------------------------------------
+This file is for running the search algorithms.
+To run a particular algorithm, run the following command:
+
+python main.py <sequence> <algorithm> <N>
+where:
+<sequence>  = "stops" or "distance"
+<algorithm> = "A*" or "Astar" or "AStar" when running A* search
+            : "ucs" or "UCS" when running uniform-cost search
+<N>         = the length of each widget (3 <= N <= 8); should be provided
+            : only if the random widget generator is being run
+-------------------------------------------------------------------------------
+"""
+
+import sys
 import search
 import helper
 
-#recipes = ["AEDCA", "BEACD", "BABCE", "DADBD", "BECBD"]
-recipes = ["AD", "AD", "AD", "BD", "BD"]
-letters = ["A", "B", "C", "D", "E"]
-
-search.AStar_MinStops(recipes, letters)
-
-#widgets = [Widget(i) for i in recipes]
-
-# # Checks random widget generator
-# for i in range(len(widgets) * 3):
-#     widgets.append(Widget())
-
-# print(widgets)
-
-# print(helper.lcsOf5List(recipes, True))
-
+# Initialize a dictionary of dictionaries to retrieve pairwise distances
+# between locations
 distances = { "A": {"B": 1064,
                     "C": 673,
                     "D": 1401,
@@ -38,3 +39,56 @@ distances = { "A": {"B": 1064,
                     "B": 337,
                     "C": 399,
                     "D": 387}}
+
+# Initialize the given recipes and the letters that can be used to form recipes
+recipes = ["AEDCA", "BEACD", "BABCE", "DADBD", "BECBD"]
+#recipes = ["AD", "AD", "AD", "BD", "BD"]
+letters = ["A", "B", "C", "D", "E"]
+
+
+if __name__ == "__main__":
+    incorrectUsageError = "Incorrect Usage: Expected " \
+                        + "\"python %s <sequence> <algorithm> " % sys.argv[0] \
+                        + "<N> \"\nOmit <N> if not running the random widget generator."
+
+    assert len(sys.argv) == 3 or len(sys.argv) == 4, incorrectUsageError
+
+    if len(sys.argv) == 4:
+        N = sys.argv[3]
+        recipes = ?
+
+    if sys.argv[1] == "stops":
+        if sys.argv[2] == "A*" or sys.argv[2] == "Astar" or sys.argv[2] == "AStar":
+            search.AStar_MinStops(recipes, letters)
+        elif sys.argv[2] == "ucs" or sys.argv[2] == "UCS":
+            search.UCS_MinStops(recipes, letters)
+        else:
+            sys.exit("AlgorithmNotRecognizedError: Is the algorithm spelled correctly?")
+
+    elif sys.argv[1] == "distance":
+        preprocessedRecipes = []
+        startFactory = "B"
+
+        for recipe in recipes:
+            newRecipe = None
+
+            if len(recipe) == 0:
+                newRecipe = ""
+            elif recipe[0] == startFactory:
+                if len(recipe) == 1:
+                    newRecipe = ""
+                else:
+                    newRecipe = recipe[1:]
+            else:
+                newRecipe = recipe[0:]
+
+            preprocessedRecipes.append(newRecipe)
+
+        if sys.argv[2] == "A*" or sys.argv[2] == "Astar" or sys.argv[2] == "AStar":
+            search.AStar_MinDistance(preprocessedRecipes, startFactory, distances, letters)
+        elif sys.argv[2] == "ucs" or sys.argv[2] == "UCS":
+            search.UCS_MinDistance(preprocessedRecipes, startFactory, distances, letters)
+        else:
+            sys.exit("AlgorithmNotRecognizedError: Is the algorithm spelled correctly?")
+    else:
+        sys.exit("SequenceNotRecognizedError: Is the sequence spelled correctly?")
--- MP2_Planning_Games/Part1/search.py
+++ MP2_Planning_Games/Part1/search.py
@@ -33,15 +33,13 @@ PART 1.1 STARTS HERE!
 # g(n) = cost so far to reach n (path cost)
 # h(n) = estimated cost from n to goal state (heuristic)
 #
-# The heuristic h(n) is computed as the longest common subsequence among
-# the sequences of remaining widget components.
+# The heuristic h(n) is computed as
 def AStar_MinStops(recipes, letters):
     # Initialize the frontier (represented as a priority queue)
     # and identify the start and goal Nodes
     frontier = []
     start = node.Node(list(recipes), "")
     goal = None
-    #trueCost = float("inf")
 
     # Mark the start with a cost of 0, compute its heuristic, initialize a counter
     # for breaking ties in the frontier, and add the start to the frontier
@@ -112,74 +110,88 @@ def AStar_MinStops(recipes, letters):
     print("Expanded Nodes: " + str(expandedNodes))
 
 
-# Searches for the food pellet using the A* algorithm.
+# Computes the factory sequence with the smallest number of stops using the A* algorithm.
 # In A*, unexpanded nodes on the frontier are sorted with a min Priority Queue.
 # The priority f(n) of a node n is computed as:
 #
 # f(n) = g(n) + h(n) , where
 #
 # g(n) = cost so far to reach n (path cost)
-# h(n) = estimated cost from n to goal (heuristic)
+# h(n) = estimated cost from n to goal state (heuristic)
 #
-# The heuristic h(n) is computed as the Manhattan distance from n to the goal.
-def AStar_MinDistance(recipes):
+# The heuristic h(n) is computed as
+def AStar_MinDistance(recipes, startFactory, distances, letters):
     # Initialize the frontier (represented as a priority queue),
     # the true path cost, and identify the start and goal Nodes
     frontier = []
-    start = maze.startingNode
-    goal = maze.food_array[0]
+    start = node.Node(list(recipes), startFactory)
+    goal = None
     trueCost = float("inf")
 
-    # Mark the start with a cost of 0, compute its heuristic, initialize a counter
-    # for breaking ties in the frontier, and add the start to the frontier
-    start.pathCost = 0
-    startHeuristic = helper.ManhattanDistance(start, goal)
+    # Mark the start with a cost of 0, initialize a counter for breaking ties
+    # in the frontier, and add the start to the frontier
+    startCost = 0
+    startHeuristic = ?
     counter = 1
-    heapq.heappush(frontier, (start.pathCost + startHeuristic, counter, maze.startingNode))
+    heapq.heappush(frontier, (startCost + startHeuristic, counter, start))
 
     # Initialize a counter to count the number of nodes that get expanded
     expandedNodes = 0
 
     while len(frontier) > 0:
         # Remove node from frontier
-        curNode = heapq.heappop(frontier)[2]
-        curNodeHeuristic = helper.ManhattanDistance(curNode, goal)
+        tup = heapq.heappop(frontier)
+        curCost = tup[0]
+        curNode = tup[2]
+        expandedNodes += 1
 
-        # Expand this node only if it hasn't been expanded (visited) yet
-        # and its value of f(n) is less than the current discovered path
-        # cost to the goal
-        if not curNode.visited and (curNode.pathCost+curNodeHeuristic) < trueCost:
-            curNode.visited = True
-            expandedNodes += 1
+        # Check if the removed node is the goal state
+        # (check if there are no components left to build in any recipe)
+        isGoalNode = True
+        for recipe in curNode.remainingRecipes:
+            if len(recipe) is not 0:
+                isGoalNode = False
+                break
 
-            # If the expanded node is the goal,
-            # compute the total path cost
-            if curNode == goal:
-                pathCost = 0
-                current = goal
+        # If the removed node is the goal state, end the search.
+        # It should be okay to end the search here because the nodes were
+        # expanded in order of increasing path cost.
+        if isGoalNode:
+            trueCost = curCost
+            goal = curNode
+            break
 
-                while current != start:
-                    pathCost += 1
-                    current = current.parent
+        for letter in letters:
+            newRemainingRecipes = []
+            isNewState = False
 
-                if pathCost < trueCost:
-                    trueCost = pathCost
+            for recipe in curNode.remainingRecipes:
+                newRecipe = None
 
-            neighbors = maze.getAdjacent(curNode)
+                if len(recipe) == 0:
+                    newRecipe = ""
+                elif recipe[0] == letter:
+                    isNewState = True
 
-            # Iterate through all the neighbors and add them to the frontier
-            # if they haven't been visited
-            for neighbor in neighbors:
-                """
-                if neighbor == goal:
-                    goal.visited = False
-                """
-                if not neighbor.visited:
-                    counter += 1
-                    helper.AStar_AddToFrontier(curNode, neighbor, goal, counter, frontier)
+                    if len(recipe) == 1:
+                        newRecipe = ""
+                    else:
+                        newRecipe = recipe[1:]
+                else:
+                    newRecipe = recipe[0:]
+
+                newRemainingRecipes.append(newRecipe)
+
+            if isNewState:
+                newNode = node.Node(newRemainingRecipes, curNode.progress + letter)
+
+                newCost = curCost + distances[curNode.progress[-1]][letter]
+                newHeuristic = ?
+                counter += 1
+                heapq.heappush(frontier, (newCost + newHeuristic, counter, newNode))
 
     print("Optimal Solution: " + goal.progress)
-    print("Minimum Number of Stops: " + str(len(goal.progress)))
+    print("Minimum Distance: " + str(trueCost))
     print("Expanded Nodes: " + str(expandedNodes))
 
 
@@ -189,30 +201,23 @@ PART 1.3 STARTS HERE!
 -------------------------------------------------------------------------------
 """
 
-# Searches for the food pellet using the A* algorithm.
-# In A*, unexpanded nodes on the frontier are sorted with a min Priority Queue.
-# The priority f(n) of a node n is computed as:
-#
-# f(n) = g(n) + h(n) , where
-#
-# g(n) = cost so far to reach n (path cost)
-# h(n) = estimated cost from n to goal (heuristic)
-#
-# The heuristic h(n) is computed as the Manhattan distance from n to the goal.
-def UCS_MinStops(recipes):
-    # Initialize the frontier (represented as a priority queue),
-    # the true path cost, and identify the start and goal Nodes
+# Computes the factory sequence with the smallest number of stops using the
+# uniform cost search algorithm.
+# In uniform cost search, unexpanded nodes on the frontier are sorted with a
+# min Priority Queue where the priority g(n) of a node n is the cost so far to
+# reach n (path cost).
+def UCS_MinStops(recipes, letters):
+    # Initialize the frontier (represented as a priority queue)
+    # and identify the start and goal Nodes
     frontier = []
-    start = maze.startingNode
-    goal = maze.food_array[0]
-    trueCost = float("inf")
+    start = node.Node(list(recipes), "")
+    goal = None
 
-    # Mark the start with a cost of 0, compute its heuristic, initialize a counter
-    # for breaking ties in the frontier, and add the start to the frontier
-    start.pathCost = 0
-    startHeuristic = helper.ManhattanDistance(start, goal)
+    # Mark the start with a cost of 0, initialize a counter for breaking ties
+    # in the frontier, and add the start to the frontier
+    startCost = 0
     counter = 1
-    heapq.heappush(frontier, (start.pathCost + startHeuristic, counter, maze.startingNode))
+    heapq.heappush(frontier, (startCost, counter, start))
 
     # Initialize a counter to count the number of nodes that get expanded
     expandedNodes = 0
@@ -220,112 +225,129 @@ def UCS_MinStops(recipes):
     while len(frontier) > 0:
         # Remove node from frontier
         curNode = heapq.heappop(frontier)[2]
-        curNodeHeuristic = helper.ManhattanDistance(curNode, goal)
+        expandedNodes += 1
 
-        # Expand this node only if it hasn't been expanded (visited) yet
-        # and its value of f(n) is less than the current discovered path
-        # cost to the goal
-        if not curNode.visited and (curNode.pathCost+curNodeHeuristic) < trueCost:
-            curNode.visited = True
-            expandedNodes += 1
+        # Check if the removed node is the goal state
+        # (check if there are no components left to build in any recipe)
+        isGoalNode = True
+        for recipe in curNode.remainingRecipes:
+            if len(recipe) is not 0:
+                isGoalNode = False
+                break
 
-            # If the expanded node is the goal,
-            # compute the total path cost
-            if curNode == goal:
-                pathCost = 0
-                current = goal
+        # If the removed node is the goal state, end the search.
+        # It should be okay to end the search here because the nodes were
+        # expanded in order of increasing path cost.
+        if isGoalNode:
+            goal = curNode
+            break
 
-                while current != start:
-                    pathCost += 1
-                    current = current.parent
+        for letter in letters:
+            newRemainingRecipes = []
+            isNewState = False
 
-                if pathCost < trueCost:
-                    trueCost = pathCost
+            for recipe in curNode.remainingRecipes:
+                newRecipe = None
 
-            neighbors = maze.getAdjacent(curNode)
+                if len(recipe) == 0:
+                    newRecipe = ""
+                elif recipe[0] == letter:
+                    isNewState = True
 
-            # Iterate through all the neighbors and add them to the frontier
-            # if they haven't been visited
-            for neighbor in neighbors:
-                """
-                if neighbor == goal:
-                    goal.visited = False
-                """
-                if not neighbor.visited:
-                    counter += 1
-                    helper.AStar_AddToFrontier(curNode, neighbor, goal, counter, frontier)
+                    if len(recipe) == 1:
+                        newRecipe = ""
+                    else:
+                        newRecipe = recipe[1:]
+                else:
+                    newRecipe = recipe[0:]
+
+                newRemainingRecipes.append(newRecipe)
+
+            if isNewState:
+                newNode = node.Node(newRemainingRecipes, curNode.progress + letter)
+
+                newCost = len(newNode.progress)
+                counter += 1
+                heapq.heappush(frontier, (newCost, counter, newNode))
 
     print("Optimal Solution: " + goal.progress)
     print("Minimum Number of Stops: " + str(len(goal.progress)))
     print("Expanded Nodes: " + str(expandedNodes))
 
 
-# Searches for the food pellet using the A* algorithm.
-# In A*, unexpanded nodes on the frontier are sorted with a min Priority Queue.
-# The priority f(n) of a node n is computed as:
-#
-# f(n) = g(n) + h(n) , where
-#
-# g(n) = cost so far to reach n (path cost)
-# h(n) = estimated cost from n to goal (heuristic)
-#
-# The heuristic h(n) is computed as the Manhattan distance from n to the goal.
-def UCS_MinDistance(recipes):
+# Computes the factory sequence with the smallest distance using the
+# uniform cost search algorithm.
+# In uniform cost search, unexpanded nodes on the frontier are sorted with a
+# min Priority Queue where the priority g(n) of a node n is the cost so far to
+# reach n (path cost).
+def UCS_MinDistance(recipes, startFactory, distances, letters):
     # Initialize the frontier (represented as a priority queue),
     # the true path cost, and identify the start and goal Nodes
     frontier = []
-    start = maze.startingNode
-    goal = maze.food_array[0]
+    start = node.Node(list(recipes), startFactory)
+    goal = None
     trueCost = float("inf")
 
-    # Mark the start with a cost of 0, compute its heuristic, initialize a counter
-    # for breaking ties in the frontier, and add the start to the frontier
-    start.pathCost = 0
-    startHeuristic = helper.ManhattanDistance(start, goal)
+    # Mark the start with a cost of 0, initialize a counter for breaking ties
+    # in the frontier, and add the start to the frontier
+    startCost = 0
     counter = 1
-    heapq.heappush(frontier, (start.pathCost + startHeuristic, counter, maze.startingNode))
+    heapq.heappush(frontier, (startCost, counter, start))
 
     # Initialize a counter to count the number of nodes that get expanded
     expandedNodes = 0
 
     while len(frontier) > 0:
         # Remove node from frontier
-        curNode = heapq.heappop(frontier)[2]
-        curNodeHeuristic = helper.ManhattanDistance(curNode, goal)
+        tup = heapq.heappop(frontier)
+        curCost = tup[0]
+        curNode = tup[2]
+        expandedNodes += 1
 
-        # Expand this node only if it hasn't been expanded (visited) yet
-        # and its value of f(n) is less than the current discovered path
-        # cost to the goal
-        if not curNode.visited and (curNode.pathCost+curNodeHeuristic) < trueCost:
-            curNode.visited = True
-            expandedNodes += 1
+        # Check if the removed node is the goal state
+        # (check if there are no components left to build in any recipe)
+        isGoalNode = True
+        for recipe in curNode.remainingRecipes:
+            if len(recipe) is not 0:
+                isGoalNode = False
+                break
 
-            # If the expanded node is the goal,
-            # compute the total path cost
-            if curNode == goal:
-                pathCost = 0
-                current = goal
+        # If the removed node is the goal state, end the search.
+        # It should be okay to end the search here because the nodes were
+        # expanded in order of increasing path cost.
+        if isGoalNode:
+            trueCost = curCost
+            goal = curNode
+            break
 
-                while current != start:
-                    pathCost += 1
-                    current = current.parent
+        for letter in letters:
+            newRemainingRecipes = []
+            isNewState = False
 
-                if pathCost < trueCost:
-                    trueCost = pathCost
+            for recipe in curNode.remainingRecipes:
+                newRecipe = None
 
-            neighbors = maze.getAdjacent(curNode)
+                if len(recipe) == 0:
+                    newRecipe = ""
+                elif recipe[0] == letter:
+                    isNewState = True
 
-            # Iterate through all the neighbors and add them to the frontier
-            # if they haven't been visited
-            for neighbor in neighbors:
-                """
-                if neighbor == goal:
-                    goal.visited = False
-                """
-                if not neighbor.visited:
-                    counter += 1
-                    helper.AStar_AddToFrontier(curNode, neighbor, goal, counter, frontier)
+                    if len(recipe) == 1:
+                        newRecipe = ""
+                    else:
+                        newRecipe = recipe[1:]
+                else:
+                    newRecipe = recipe[0:]
+
+                newRemainingRecipes.append(newRecipe)
+
+            if isNewState:
+                newNode = node.Node(newRemainingRecipes, curNode.progress + letter)
+
+                newCost = curCost + distances[curNode.progress[-1]][letter]
+                counter += 1
+                heapq.heappush(frontier, (newCost, counter, newNode))
 
     print("Optimal Solution: " + goal.progress)
-    print("Minimum Number of Stops: " + str(len(goal.progress)))
+    print("Minimum Distance: " + str(trueCost))
     print("Expanded Nodes: " + str(expandedNodes))
