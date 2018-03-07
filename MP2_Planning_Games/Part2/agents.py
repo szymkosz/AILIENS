00b80e005c7bfb8f054a8dfea620eb2d7ba99c14
szymkosz <szymkosz@icloud.com>
2018-03-07 12:50:33 -0600

    Continued implementation of Agent and Reflex(Agent) classes

--- MP2_Planning_Games/Part2/agents.py
+++ MP2_Planning_Games/Part2/agents.py
@@ -3,7 +3,7 @@ from board import Board
 from position import Position
 
 class Agent:
-    def __init__(self, game, playerNum):
+    def __init__(self, game=Gomoku(), playerNum=1):
         self.name = "AGENT"
         self.game = game
         self.playerNum = playerNum
@@ -17,28 +17,37 @@ class Agent:
 
 class Reflex(Agent):
     def __init__(self, game, playerNum):
+        super().__init__(game, playerNum)
         self.name = "REFLEX"
-        self.game = game
-        self.playerNum = playerNum
-        self.player = Gomoku.players[(playerNum-1)%2]
 
     def makeMove(self):
         raise NotImplementedError()
 
     def getMove(self):
         patterns = self.game.getPatterns()
-        curPattern = (Gomoku.players[playerNum%2], 4, False) ## Opponent, open, 4-in-a-row
+        curPattern = (self.player, 4, False) ## Agent, 4-in-a-row, open
         count = patterns[0][curPattern]
 
         ## 1. Check whether the agent can win the game by placing one stone
         #      Break a tie by choosing a move in the following order:
         #           left > down > right > up
         if count > 0:
-            curPatternVector = patterns[1][curPattern]
-            prevPos
+            # Only one move is needed to win the game, return that move.
+            allPossibleMoves = patterns[2][curPattern]
+            # TODO: Break a potential tie. Check how moves are sorted.
+            nextCoord = allPossibleMoves[0][0]
+            return nextCoord
 
         ## 2. Check whether the opponent has an unbroken chain of 4 stones and an
         #      empty position that could win the game. Place a stone in that position.
+        curPattern = (Gomoku.players[playerNum%2], 4, False)
+        count = patterns[0][curPattern]
+        if count > 0:
+            # Only one move is needed to win, so if there are more than one possible,
+            #  the game would be lost anyway, so just get the first possible one.
+            allPossibleMoves = patterns[2][curPattern]
+            nextCoord = allPossibleMoves[0][0]
+            return nextCoord
 
         ## 3. Check whether the opponent has an unbroken chain of 3 stones and an
         #      empty position on both ends. Break a tie by choosing a move in the
