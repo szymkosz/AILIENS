c4e7a2f4c5db175df152bc5afc257a60a5629e23
Nikita Jain <07njain@gmail.com>
2018-03-09 21:50:29 -0600

    gui working w/o victory text

--- MP2_Planning_Games/Part2/Agents/human.py
+++ MP2_Planning_Games/Part2/Agents/human.py
@@ -1,31 +1,66 @@
 import pygame
 import time
-from gui import screenToBoard
+#from gui import screenToBoard
 from gomoku import Gomoku
 from position import Position
-from agent import Agent
+from Agents.agent import Agent
 
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
 blue = (0, 0, 255)
 
+coordinateList = [50, 160, 270, 380, 490, 600, 710]
+
 class Human(Agent):
 	def __init__(self, game=Gomoku(), playerNum=1):
+		# super(self).__init__(game, playerNum)
 		super().__init__(game, playerNum)
 		self.name = "HUMAN"
 
 	def makeMove(self):
-		raise NotImplementedError()
+		x, y = self.getMove()
+		if self.playerNum == 1:
+			self.game.setPiece(x, y, 1)
+		elif self.playerNum == 2:
+			self.game.setPiece(x, y, 0)
+
+
 
 	# returns coordinates
 	def getMove(self):
 		# get user clicking input
-		for event in pygame.event.get():
-			if event.type == pygame.MOUSEBUTTONUP:
-				screenX, screenY = pygame.mouse.get_pos()
-				boardX, boardY = screenToBoard(screenX, screenY)
-				return (boardX, boardY)
-				
+		while(True):
+			for event in pygame.event.get():
+				if event.type == pygame.MOUSEBUTTONUP:
+					screenX, screenY = pygame.mouse.get_pos() # ??
+					boardX, boardY = screenToBoard(screenX, screenY)
+					if(boardX == -1 or boardY == -1):
+						pass
+					else:
+						return (boardX, boardY)
 
+				if event.type == pygame.QUIT:
+					pygame.display.quit()
+					pygame.quit()
+					quit()
+				
+# returns tuple of board coordinates
+def screenToBoard(x, y):
+	print("screenToBoard: " + str(x) + ", " + str(y))
+	if(x<50 or y<50 or x>820 or y>820):
+		return (-1, -1);
+
+	boardX = 0
+	boardY = 0
+	for i in range(0, 7):
+		if x > coordinateList[6-i]:
+			boardX = 6-i 
+			break
+	for j in range(0, 7):
+		if  y > coordinateList[6-j]:
+			boardY = 6-j
+			break
+
+	return (boardX, boardY)
 
--- MP2_Planning_Games/Part2/gui.py
+++ MP2_Planning_Games/Part2/gui.py
@@ -1,12 +1,10 @@
 import pygame
 import time
 from gomoku import Gomoku
-from alphabeta import AlphaBeta 
-from human import Human
-from minimax import MiniMax 
-from reflex import Reflex
-
-# parse the board instead of looking for x, y input
+# from alphabeta import AlphaBeta 
+from Agents.human import Human
+# from minimax import MiniMax 
+# from reflex import Reflex
 
 # colors
 white = (255, 255, 255)
@@ -16,96 +14,98 @@ blue = (0, 0, 255)
 
 coordinateList = [50, 160, 270, 380, 490, 600, 710]
 
-class GUI(object):
-	boardInitialized = False
-	gameDisplay = None
-	keepPlaying = True
-	gameExit = False
-	win = False
+boardInitialized = False
+gameDisplay = None
+keepPlaying = True
+gameExit = False
+win = False
 
-	def __init__(self, game=Gomoku())
-		# check if the board has been initalized 
-		if boardInitialized == False:
-			pygame.init()
-			gameDisplay = pygame.display.set_mode((870, 870))
-			pygame.display.set_caption('Gomoku')
-			# draw board
-			gameDisplay.fill(white)
-			for x in range(0, 8):
-				pygame.draw.rect(gameDisplay, black, [110*x + 50, 50, 10, 770])
-			for y in range(0, 8):
-				pygame.draw.rect(gameDisplay, black, [50, 110*y + 50, 770, 10])
-			pygame.display.update()
-			# font
-			bigFont = pygame.font.SysFont(None, 100)
-			littleFont = pygame.font.SysFont(None, 25)
-			boardInitialized = True
+game = Gomoku()
+Jon = Human(game, 1)
+Jess = Human(game, 2)
+# check if the board has been initalized 
+if boardInitialized == False:
+	pygame.init()
+	pygame.display.init()
+	gameDisplay = pygame.display.set_mode((870, 870))
+	pygame.display.set_caption('Gomoku')
+	# draw board
+	gameDisplay.fill(white)
+	# pygame.display.update() #
+	#time.sleep(10) #
+	for x in range(0, 8):
+		pygame.draw.rect(gameDisplay, black, [110*x + 50, 50, 10, 780])
+	for y in range(0, 8):
+		pygame.draw.rect(gameDisplay, black, [50, 110*y + 50, 780, 10])
+	pygame.display.update()
+	# time.sleep(10) #
+	# font
+	bigFont = pygame.font.SysFont(None, 200)
+	littleFont = pygame.font.SysFont(None, 35)
+	boardInitialized = True
+	# gameLoop(self)
 
-	def gameLoop(self):
-		while not gameExit:
-			if keepPlaying == True:
-				if Gomoku.reds_turn:
-					win = Jon.makeMove()
-				else:
-					win = Jess.makeMove()
+# returns tuple of coordinates at center of the square
+def boardToScreen(x, y):
+	if(x<0 or y<0 or x>6 or y>6):
+		return -1;
+
+	screenX = 110*x + 105;
+	screenY = 110*y + 105;
+	return (screenX, screenY)
+
+def refreshBoard():
+	# parse the board to get coordinates
+	board = game.board
+	for x in range(0, 7):
+		for y in range(0, 7):
+			pos = board[x][y]
+			if not pos.char == '.':
+				# print the piece with the corresponding colors
+				screenX, screenY = boardToScreen(x, y)
+				if pos.color == "RED":
+					pygame.draw.circle(gameDisplay, red, (screenX+5, screenY+5), 40)
+				if pos.color == "BLUE":
+					pygame.draw.circle(gameDisplay, blue, (screenX+5, screenY+5), 40)
+				# print the corresponding character on the piece
+				char = pos.char
+				pieceChar = littleFont.render(char, True, white)
+				gameDisplay.blit(pieceChar, [screenX-2, screenY-5])
 					
-				self.refreshBoard()
+	# refresh the board display
+	pygame.display.update()
 
-			# if there is a winner, print winner to screen
-			if win == 1:
-				if Gomoku.reds_turn:
-					text = bigFont.render("Blue wins!", True, blue)
-				else:
-					text = bigFont.render("Red wins!", True, red)
-				gameDisplay.blit(text, [385, 385])
-				pygame.display.update()
-				keepPlaying = False
-
-			if event.type == pygame.QUIT:
-				gameExit = True
+while not gameExit:
+	if keepPlaying == True:
+		print(game.reds_turn)
+		if game.reds_turn:
+			win = Jon.makeMove()
+		else:
+			win = Jess.makeMove()
 					
-		pygame.quit()
-		quit()
+		refreshBoard()
 
-	def refreshBoard()
-		# parse the board to get coordinates
-		board = Gomoku.board
-		for x in range(0, 7):
-			for y in range(0, 7):
-				pos = board[x][y]
-				if not pos.char == '.':
-					# print the piece with the corresponding colors
-					screenX, screenY = self.boardToScreen(x, y)
-					if pos.color == "RED":
-						pygame.draw.circle(gameDisplay, red, (screenX, screenY), 40)
-					if pos.color == "BLUE":
-						pygame.draw.circle(gameDisplay, blue, (screenX, screenY), 40)
-					# print the corresponding character on the piece
-					char = pos.char
-					pieceChar = littleFont.render(char, True, white)
-					gameDisplay.blit(pieceChar, [screenX, screenY])
-					
-		# refresh the board display
+	# if there is a winner, print winner to screen
+	if win == True:
+		if game.reds_turn:
+			text = bigFont.render("Blue wins!", True, blue)
+		else:
+			text = bigFont.render("Red wins!", True, red)
+		gameDisplay.blit(text, [385, 385])
 		pygame.display.update()
+		keepPlaying = False
 
-	# returns tuple of coordinates at center of the square
-	def boardToScreen(self, x, y):
-		if(x<0 or y<0 or x>6 or y>6):
-			return -1;
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			gameExit = True
+				
+if gameExit == True:	
+	pygame.display.quit()
+	pygame.quit()
+	quit()
 
-		screenX = 110*x + 105;
-		screenY = 110*y + 105;
-		return (screenX, screenY)
+# refreshBoard
 
-	# returns tuple of board coordinates
-	def screenToBoard(self, x, y):
-		if(x<50 or y<50 or x>820 or y>820):
-			return -1;
+# boardToScreen
 
-		for i in range(0, 7):
-			if x > coordinateList[6-i]:
-				boardX = 6-i 
-			if  y > coordinateList[6-i]:
-				boardY = 6-i
-
-		return (boardX, boardY)
+# screenToBoard
