6b717d26f1322d9889139d604f37c00e9c56e017
szymkosz <szymkosz@icloud.com>
2018-03-11 01:30:03 -0600

    All but Rule 4 implemented

--- MP2_Planning_Games/Part2/Agents/reflex.py
+++ MP2_Planning_Games/Part2/Agents/reflex.py
@@ -1,6 +1,7 @@
 from gomoku import Gomoku
 from position import Position
-from agent import Agent
+from Agents.agent import Agent
+import random
 
 class Reflex(Agent):
     def __init__(self, game=Gomoku(), playerNum=1):
@@ -8,43 +9,82 @@ class Reflex(Agent):
         self.name = "REFLEX"
 
     def makeMove(self):
-        raise NotImplementedError()
+        moveToMake = self.getMove()
+
+        # The agent is player1/RED
+        settingRed = self.game.players[0] == self.player
+
+        return self.game.setPiece(moveToMake[0], moveToMake[1], settingRed)
 
     def getMove(self):
         patterns = self.game.getPatterns()
-        curPattern = (self.player, 4, False) ## Agent, 4-in-a-row, open
+        curPattern = (self.player, 4, True) ## Agent, 4-in-a-row, open
         count = patterns[0][curPattern]
+
+        def unwrapList(toUnwrap, unwrapped):
+            if type(toUnwrap) == type([]):
+                for value in toUnwrap:
+                    unwrapList(value, unwrapped)
+            else:
+                unwrapped.append(toUnwrap)
+
+        def breakTie(moves):
+            unwrappedMoves = []
+            unwrapList(moves, unwrappedMoves)
+            unwrappedMoves = sorted(unwrappedMoves)
+            return unwrappedMoves[0]
+
+
+        # print("curPattern: ", curPattern)
+        # print("Count: ", count)
 
         ## 1. Check whether the agent can win the game by placing one stone
         #      Break a tie by choosing a move in the following order:
         #           left > down > right > up
         if count > 0:
+            # print("\tin first rule")
             # Only one move is needed to win the game, return that move.
-            allPossibleMoves = patterns[2][curPattern]
-            # TODO: Break a potential tie. Check how moves are sorted.
-            nextCoord = allPossibleMoves[0][0]
+            allPossibleMoves = []
+            unwrapList(patterns[2][curPattern][True], allPossibleMoves)
+            allPossibleMoves = sorted(allPossibleMoves)
+            nextCoord = allPossibleMoves[0]
             return nextCoord
 
         ## 2. Check whether the opponent has an unbroken chain of 4 stones and an
         #      empty position that could win the game. Place a stone in that position.
-        curPattern = (Gomoku.players[playerNum%2], 4, False)
+        curPattern = (Gomoku.players[self.playerNum % 2], 4, True)
         count = patterns[0][curPattern]
+        # print("curPattern: ", curPattern)
+        # print("Count: ", count)
         if count > 0:
+            # print("\tin second rule")
             # Only one move is needed to win, so if there are more than one possible,
             #  the game would be lost anyway, so just get the first possible one.
-            allPossibleMoves = patterns[2][curPattern]
-            nextCoord = allPossibleMoves[0][0]
+            allPossibleMoves = []
+            unwrapList(patterns[2][curPattern][True], allPossibleMoves)
+            allPossibleMoves = sorted(allPossibleMoves)
+            nextCoord = allPossibleMoves[0]
             return nextCoord
 
         ## 3. Check whether the opponent has an unbroken chain of 3 stones and an
         #      empty position on both ends. Break a tie by choosing a move in the
         #      following order:
         #           left > down > right > up
-        curPattern = (Gomoku.players[playerNum%2], 3, False)
+        curPattern = (Gomoku.players[self.playerNum % 2], 3, True)
         count = patterns[0][curPattern]
-        allPossibleMoves = patterns[2][curPattern]
-        while count > 0:
-            pass
+        # print("curPattern: ", curPattern)
+        # print("Count: ", count)
+        if count > 0:
+            # print("\tin third rule")
+            allPossibleMoves = []
+            listToUnwrap = []
+            for i in patterns[2][curPattern][True]:
+                if len(i) > 1:
+                    listToUnwrap.append(i)
+            unwrapList(listToUnwrap, allPossibleMoves)
+            allPossibleMoves = sorted(allPossibleMoves)
+            nextCoord = allPossibleMoves[0]
+            return nextCoord
 
         ## 4. Otherwise, find the winning block (a block of 5 consecutive spaces
         #      in which victory is still possible) containing the largest number of
@@ -54,8 +94,17 @@ class Reflex(Agent):
         #      among those which are farthest to the left, find the position
         #      which is closest to the bottom.
 
+
+
         ## 5. The first move can follow any of the following strategies:
         #       -   Follow rule #4 above (and choose bottom left corner)
         #       -   Play at random
         #       -   Make an optimal move (in the middle)
-        raise NotImplementedError()
+        x = round((self.game.dim - 1) * random.random())
+        y = round((self.game.dim - 1) * random.random())
+        while ((x,y) in self.game.movesTaken):
+            x = round((self.game.dim - 1) * random.random())
+            y = round((self.game.dim - 1) * random.random())
+        nextCoord = (x,y)
+        return nextCoord
+        # raise NotImplementedError()
