8ca77b0804cca4d5cd110de39f65b3da8feb1ef5
tdog191 <twarther17@gmail.com>
2018-04-13 18:59:24 -0500

    Fixed the loader file to correctly load the weights and updated the documentation

--- MP4_Reinforcement_Learning/loader.py
+++ MP4_Reinforcement_Learning/loader.py
@@ -1,52 +1,62 @@
 import numpy as np
 
+TRAINING_OUTPUT_FILENAME = "training_results.npz"
+
+
 """
-The parser function takes in the name of a file containing digit data
-and returns a 3-tuple of the digit images, digit images organized by class label,
-and the class labels.  The details of the returned tuple are as follows:
+The parser function takes in the name of a file containing the states and actions
+of an expert Pong game and returns a 2-tuple of the game states and the expert's
+corresponding actions.  The details of the returned tuple are as follows:
 
-images (0th entry): Let T be the total number of images in the file represented
-                    by "fileName".  Then this entry is a 1,024 x T numpy array
-                    where every column is the bitmap of an individual image.
+states (0th entry): Let T be the total number of game states in the file represented
+                    by "fileName".  Then this entry is a T x 5 numpy array
+                    where every row contains ball_x, ball_y, velocity_x, velocity_y,
+                    and paddle_y in this order for an individual game state.
 
-images_by_class:    Let n_k be the number of images in the file labeled as class k.
-(1th entry)         Then this entry is a list such that the kth entry is a 1,024 x n_k
-                    numpy array where every column is the bitmap of an individual image
-                    belonging to class k.
-
-labels (2th entry): Let T be the total number of images in the file represented
-                    by "fileName".  Then this entry is a 1D numpy array containing
-                    all T classification labels.  The ith entry is the label for
-                    the ith image in the file.
+actions:            Let T be the total number of game states in the file represented
+(1th entry)         by "fileName".  Then this entry is a T-dimensional numpy vector
+                    such that the ith entry contains the expert's action in the ith
+                    game state.  This entry is 0 if the expert moves the paddle down,
+                    1 if the expert doesn't move the paddle, and 2 if the expert moves
+                    the paddle up.
 """
 def parser(fileName):
     return loadFile(fileName)
 
+
 """
 The loadFile function is a helper function for parser responsible for
-parsing the file for the images and classification labels.  See the details of the
-first two returned entries in the documentation of parser to understand what this
-function returns.
+parsing the file for the game states and the expert's corresponding actions.
+See the details of the two returned entries in the documentation of parser to
+understand what this function returns.
 """
 def loadFile(fileName):
     file = open(fileName, 'r')
     data = np.empty((0,6), dtype=np.float64)
+
     while True:
         try:
             line = np.asarray((file.readline()[:-1]).split(' '), dtype=np.float64)
             data = np.vstack((data,line))
         except:
             break
+
     states = data[:,:-1]
     actions = data[:,-1]
     return (states, actions)
 
 
+"""
+This function takes in a list of weights and a list of biases and saves them to
+a .npz file.  The ith entry of both of these lists corresponds to the 2D numpy
+matrix of weights and numpy vector of biases respectively of the ith layer of
+the neural network.
 
-
-training_output_filename = "training_results.npz"
-
-def save_training_results_to_file(weights, biases):
+The save_training_results_to_file and load_training_results_from_file functions
+are used to efficiently store and retrieve the weights and biases of the neural
+network after they are obtained through training.
+"""
+def save_training_results_to_file(weights, biases, fileName=TRAINING_OUTPUT_FILENAME):
     reshaped_weights = []
     weight_shapes = []
 
@@ -54,42 +64,24 @@ def save_training_results_to_file(weights, biases):
         reshaped_weights.append(matrix.flatten())
         weight_shapes.append(matrix.shape)
 
-    np.savez(training_output_filename, weights=reshaped_weights, weight_shapes=weight_shapes, biases=biases)
+    np.savez(fileName, weights=reshaped_weights, weight_shapes=weight_shapes, biases=biases)
 
-def load_training_results_from_file():
-    data = np.load(training_output_filename)
 
+"""
+This function loads a list of weights and a list of biases from a .npz file
+and returns them.  The ith entry of both of these lists corresponds to the 2D numpy
+matrix of weights and numpy vector of biases respectively of the ith layer of
+the neural network.
+
+The save_training_results_to_file and load_training_results_from_file functions
+are used to efficiently store and retrieve the weights and biases of the neural
+network after they are obtained through training.
+"""
+def load_training_results_from_file(fileName=TRAINING_OUTPUT_FILENAME):
+    data = np.load(fileName)
     original_weights = []
-    print(data['weights'])
-    print("These are shapes")
-    print(data['weight_shapes'])
 
-    for s in data['weight_shapes']:
-
-    for matrix, shape in (data['weights'], data['weight_shapes']):
-        print(matrix)
-        print("This is a shape")
-        print(shape)
+    for matrix, shape in zip(data['weights'], data['weight_shapes']):
         original_weights.append(matrix.reshape(tuple(shape)))
 
     return (original_weights, data['biases'])
-
-w = np.arange(9).reshape(3,3)
-w2 = np.arange(15).reshape(3,5)
-print(w)
-print(w2)
-
-weights = [w, w2]
-biases = [np.arange(20), np.arange(5)]
-
-print(weights)
-print(biases)
-save_training_results_to_file(weights, biases)
-orig_weights, orig_biases = load_training_results_from_file()
-print(orig_weights, orig_biases)
-"""
-print(data.files)
-print(data['weights'])
-print(data['weight_shapes'])
-print(data['biases'])
-"""
