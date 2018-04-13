39672674fd732b9c4991601853abdd84654807bc
szymkosz <szymkosz@icloud.com>
2018-04-13 17:00:41 -0500

    Finished parser function

--- MP4_Reinforcement_Learning/main.py
+++ MP4_Reinforcement_Learning/main.py
@@ -28,38 +28,43 @@ where:
 
 #import numpy as np
 import sys
+from loader import parser
 
-if __name__ == "__main__":
-    """
-    incorrectUsageError = "Incorrect Usage: Expected " \
-                        + "\"python %s <bayes> <laplace>\" or " % sys.argv[0] \
-                        + "\"python %s <perceptron> <hasBias> <weightsAreRandom> <hasRandomTrainingOrder> <epochs>\"" % sys.argv[0]
-    """
-    incorrectUsageError = "Incorrect Usage: Expected " \
-                        + "\"python %s <bayes> <laplace>\" or " % sys.argv[0] \
-                        + "\"python %s <perceptron> <learning_rate_power> <epochs>\"" % sys.argv[0]
+fileName = "Data/expert_policy.txt"
 
-    assert len(sys.argv) >= 3, incorrectUsageError
+parser(fileName)
 
-    if sys.argv[1].lower() == "part1":
-        # Run part 1
-        
-
-        # Check the validity of the command-line arguments
-        assert len(sys.argv) in [3,4], incorrectUsageError
-
-    elif sys.argv[1].lower() == "part2":
-        # Run part 2
-        if sys.argv[2].lower() == "best":
-            # Run code for reproducing the best empirical results
-            # discovered for the non-differentiable perceptron
-            perceptron.reproduce_best_results(training_data_tuple, test_data_tuple, False)
-        else:
-            # Check that the number of command-line arguments is correct
-            assert len(sys.argv) == 4, incorrectUsageError
-
-            # Run code for training and classifying with perceptron
-            perceptron.run_perceptron(training_data_tuple, test_data_tuple, False, int(sys.argv[2]), int(sys.argv[3]))
-
-    else:
-        sys.exit("INVALID ARGUMENT ERROR: The third argument must be \"bayes\", \"naivebayes\", \"perceptron\", or \"face\" (ignoring case)!")
+# if __name__ == "__main__":
+#     """
+#     incorrectUsageError = "Incorrect Usage: Expected " \
+#                         + "\"python %s <bayes> <laplace>\" or " % sys.argv[0] \
+#                         + "\"python %s <perceptron> <hasBias> <weightsAreRandom> <hasRandomTrainingOrder> <epochs>\"" % sys.argv[0]
+#     """
+#     incorrectUsageError = "Incorrect Usage: Expected " \
+#                         + "\"python %s <bayes> <laplace>\" or " % sys.argv[0] \
+#                         + "\"python %s <perceptron> <learning_rate_power> <epochs>\"" % sys.argv[0]
+#
+#     assert len(sys.argv) >= 3, incorrectUsageError
+#
+#     if sys.argv[1].lower() == "part1":
+#         # Run part 1
+#
+#
+#         # Check the validity of the command-line arguments
+#         assert len(sys.argv) in [3,4], incorrectUsageError
+#
+#     elif sys.argv[1].lower() == "part2":
+#         # Run part 2
+#         if sys.argv[2].lower() == "best":
+#             # Run code for reproducing the best empirical results
+#             # discovered for the non-differentiable perceptron
+#             perceptron.reproduce_best_results(training_data_tuple, test_data_tuple, False)
+#         else:
+#             # Check that the number of command-line arguments is correct
+#             assert len(sys.argv) == 4, incorrectUsageError
+#
+#             # Run code for training and classifying with perceptron
+#             perceptron.run_perceptron(training_data_tuple, test_data_tuple, False, int(sys.argv[2]), int(sys.argv[3]))
+#
+#     else:
+#         sys.exit("INVALID ARGUMENT ERROR: The third argument must be \"bayes\", \"naivebayes\", \"perceptron\", or \"face\" (ignoring case)!")
